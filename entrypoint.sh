fc82a56a7eaa46ebfb0ae2faa0c68f633a095175
wallrony <wrony.neves@quimbik.com>
2022-08-31 10:39:24 -0300

    Ask grant access for ronypsql

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ git diff --name-only HEAD..HEAD~1 | while read line; do
         raw_user_email=$(git log -p -- $line | grep Author | head -1)
         user_email=$(echo $raw_user_email | awk 'match($0, /<.*>/) { print substr($0, RSTART+1, RLENGTH-2) }')
         echo "About to grant temporary access to $user_email on $resource_name"
-        python3 /main.py $resource_name $user_email
+        python3 /main.py "$resource_name" "$user_email"
     fi
 done
