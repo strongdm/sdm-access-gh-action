ddd5048b2e6e508a89a548509468656dffa77bdb
wallrony <wrony.neves@quimbik.com>
2022-08-31 11:31:07 -0300

    Add debug output on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,10 @@ git checkout main
 git pull
 git diff --name-only HEAD..HEAD~1 | while read line; do 
     resource_name=$(echo $line | awk '/^resources/ { gsub("resources/", "", $0); print $0 }')
-    raw_user_email=$(git log -p -- $line | grep Author | head -1)
-    git log -p -- $line | grep Author | head -1
-    echo "Raw user email: $raw_user_email"
-    user_email=$(echo $raw_user_email | awk 'match($0, /<.*>/) { print substr($0, RSTART+1, RLENGTH-2) }')
     if [ "$resource_name" != "" ]; then
+        raw_user_email=$(git log -p -- "$line" | grep Author | head -1)
+        user_email=$(echo $raw_user_email | awk 'match($0, /<.*>/) { print substr($0, RSTART+1, RLENGTH-2) }')
         echo "About to grant temporary access to $user_email on $resource_name"
-        # python3 /main.py "$resource_name" "$user_email"
+        python3 /main.py "$resource_name" "$user_email"
     fi
 done
