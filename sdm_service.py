80116cadb0b547366cf6cf9845115b5e27cd59ea
wallrony <wrony.neves@quimbik.com>
2022-08-31 10:38:06 -0300

    Fix sdm host port

--- sdm_service.py
+++ sdm_service.py
@@ -5,7 +5,7 @@ def create_sdm_service(api_access_key, api_secret_key, log):
     client = strongdm.Client(
         api_access_key,
         api_secret_key,
-        host="ec2-35-87-197-41.us-west-2.compute.amazonaws.com:5001",
+        host="ec2-35-87-197-41.us-west-2.compute.amazonaws.com:5000",
         insecure=True
     )
     return SdmService(client, log)
